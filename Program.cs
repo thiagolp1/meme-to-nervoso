031815efb3653bd717c965829354182dc7cfe64b
thiago <e107ti@cps.sp.gov.br>
2022-03-18 22:40:06 -0300

    terminei a liçao

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿String fraseD, frasememe;
+
+Console.Write("Usuário, o que você pensa sobre mim? ");
+fraseD = Console.ReadLine()!;
+
+frasememe = $"{fraseD.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+
+Console.WriteLine(frasememe);
